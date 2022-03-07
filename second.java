8b6a2de4b61580a236318d50bf4c8f2847bf95fc
Himanshu <himanshu372singh>
2022-03-07 09:17:52 +0530

    change some detail

--- second.java
+++ second.java
@@ -1,5 +1,5 @@
 public class second {
     public static void main(String[] args) {
-        System.out.println("hello second world");
+        System.out.println("hello second world part 2");
     }
 }
